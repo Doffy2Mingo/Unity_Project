87a2babf1f09aaeafa5ca6d58927db271e083e4d
Doffy2Mingo <bouras.farid.2002@gmail.com>
2024-05-01 09:13:27 +0200

    Project

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; set; }
+
+    public AudioSource JumpSound;
+    public AudioSource WalkSound;
+    public AudioSource BackgroundMusic;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void PlaySound(AudioSource soundToPlay)
+    {
+        if (!soundToPlay.isPlaying)
+        {
+            soundToPlay.Play();
+        }
+    }
+
+
+}
